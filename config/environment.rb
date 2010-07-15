5cc955ea75d65c0fc6d1d3fded581a5aa44c1dbe
Micah Winkelspecht <micah@wks172.local>
2010-07-14 17:51:14 -0700

    Changing gems around.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,9 +19,18 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com'
+  config.gem 'gemcutter'
+  config.gem 'tumble'
+  config.gem 'aasm'
   config.gem 'haml'
-  config.gem 'authlogic'
+  config.gem 'clearance'
+  config.gem 'high_voltage'
+  config.gem 'pacecar'
+  config.gem 'paperclip'
+  config.gem 'will_paginate'
+  config.gem 'formtastic'
+  config.gem 'validation_reflection'
+  config.gem 'hoptoad_notifier'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
